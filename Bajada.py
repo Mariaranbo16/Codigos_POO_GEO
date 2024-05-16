c8b24aa2caddedcbbabc5ece0dec08df3a44592f
Mariana Arango <marangob@udistrital.edu.co>
2024-05-15 19:12:13 -0500

    VRS FINAL _PRESENTACIÓN

--- Bajada.py
+++ Bajada.py
@@ -7,54 +7,54 @@ class NivelacionDiferencialGeodesica:
         self.ejey = []
 
     def nivelacion_instrumental(self):
-        Numero_Estaciones = int(input("Inserta El Numero De Estaciones: "))
-        Estacion1 = input("Inserta El Nombre Del Punto 1: ")
+        Numero_Estaciones = int(input("Numero de estaciones: "))
+        Estacion1 = input("Nombre de la estación: ")
         VistaMas = round(float(input("V+: ")), 6)
-        CotaIni = round(float(input("Inserta La Cota Del Punto: ")), 6)
+        CotaIni = round(float(input("Cota de la estación: ")), 6)
         Hi = round(CotaIni + VistaMas, 6)
         self.todos = [[Estacion1, CotaIni]]
         self.ejex = [0]
         self.ejey = [CotaIni]
         for _ in range(Numero_Estaciones - 1):
-            print("¿Tiene Vista Intermedia?: ")
+            print("¿Tiene vista intermedia?: ")
             Tiene = int(input("Si(1) o No(2): "))
             if Tiene == 1:
-                NumVI = int(input("¿Cuantas VI Tienes?: "))
+                NumVI = int(input("¿Cuantas VI tiene?: "))
                 for _ in range(NumVI):
-                    VistaIntermedia = input("Inserta El Nombre De La VI: ")
-                    VistaInter = round(float(input("Inserta el valor de VI: ")), 6)
+                    VistaIntermedia = input("Nombre de la VI: ")
+                    VistaInter = round(float(input("Valor de VI: ")), 6)
                     CotaVi = round(Hi - VistaInter, 6)
                     self.todos.append([VistaIntermedia, CotaVi])
-            Estacion = input("Inserta El Nombre Del Punto: ")
+            Estacion = input("Nombre del cambio: ")
             VistaMas = round(float(input("V+: ")), 6)
             VistaMenos = round(float(input("V-: ")), 6)
             CotaEste = round(Hi - VistaMenos, 6)
             HiEste = round(CotaEste + VistaMas, 6)
             Hi = round(HiEste, 6)
             self.todos.append([Estacion, CotaEste])
-            Distancia = round(float(input("Inserte la Distancia entre estas dos estaciones: ")), 6)
+            Distancia = round(float(input("Distancia entre las estaciones: ")), 6)
             Calculada = round(Distancia + self.ejex[-1], 6)
             self.ejex.append(Calculada)
             self.ejey.append(CotaEste)
         self.mostrar_resultados()
 
     def nivelacion_subes_bajas(self):
-        Numero_Estaciones = int(input("Inserta El Numero De Estaciones: "))
-        Estacion1 = input("Inserta El Nombre De La Estacion:")
+        Numero_Estaciones = int(input("Numero de estaciones: "))
+        Estacion1 = input("Nombre de la estacion: ")
         DistanciaE1 = 0
         VistaMas1 = round(float(input("V+: ")), 6)
-        CotaIni = round(float(input("Inserta La Cota De La Estacion:")), 6)
+        CotaIni = round(float(input("Cota de la estacion: ")), 6)
         self.todos = [[Estacion1, CotaIni]]
         self.ejex = [DistanciaE1]
         self.ejey = [CotaIni]
         for _ in range(Numero_Estaciones - 1):
-            print("Tiene Vista Intermedia?")
+            print("Tiene vista intermedia?")
             Tiene = float(input("¿Si(1) o No(2)?: "))
             if Tiene == 1:
-                NumVI = int(input("¿Cuantas Tienes?: "))
+                NumVI = int(input("¿Cuantas tiene?: "))
                 for _ in range(NumVI):
-                    VistaIntermedia = input("Inserta El Nombre De La VI: ")
-                    VistaInter = round(float(input("Inserta el valor de VI: ")), 6)
+                    VistaIntermedia = input("Nombre de la VI: ")
+                    VistaInter = round(float(input("Valor de VI: ")), 6)
                     SubeOBaja = round(VistaMas1 - VistaInter, 6)
                     if SubeOBaja < 0:
                         CotaVi = round(CotaIni - abs(SubeOBaja), 6)
@@ -64,7 +64,7 @@ class NivelacionDiferencialGeodesica:
                     else:
                         CotaIntermedia = round(CotaIni + SubeOBaja, 6)
                         self.todos.append([VistaIntermedia, round(SubeOBaja, 6), CotaIntermedia])
-            Estacion = input("Inserta El Nombre De La Estacion: ")
+            Estacion = input("Nombre del cambio: ")
             VistaMas = round(float(input("V+: ")), 6)
             VistaMenos = round(float(input("V-: ")), 6)
             SubeOBaja = round(VistaMas1 - VistaMenos, 6)
@@ -79,7 +79,7 @@ class NivelacionDiferencialGeodesica:
                 self.ejey.append(CotaEste)
                 CotaIni = round(CotaEste, 6)
                 self.todos.append([Estacion, round(SubeOBaja, 6), round(CotaEste, 6)])
-            Distancia = round(float(input("Inserte la Distancia entre estas dos estaciones: ")), 6)
+            Distancia = round(float(input("Distancia entre las estaciones: ")), 6)
             Calculada = round(Distancia + self.ejex[-1], 6)
             self.ejex.append(Calculada)
         self.mostrar_resultados()
@@ -92,7 +92,7 @@ class NivelacionDiferencialGeodesica:
         print("*" * 28)
         plt.plot(self.ejex, self.ejey, linestyle='-', color='green')
         plt.xlabel("Distancia En M")
-        plt.ylabel("Altura En M (A.S.N.M)")
+        plt.ylabel("Altura En M")
         plt.grid(True) 
         plt.title("Gráfico de Nivelación")  
         plt.show()
@@ -100,10 +100,10 @@ class NivelacionDiferencialGeodesica:
 
 def main():
     print("*" * 30)
-    print("Programa Nivelacion Diferencial Geodesica")
+    print("Programa Nivelacion")
     print("¿Cual Metodo Desea Utilizar?")
-    print("1. Nivelacion Altura Instrumental")
-    print("2. Nivelacion Subes Y Bajas")
+    print("1. Altura Instrumental")
+    print("2. Subes Y Bajas")
     seleccion = float(input("Tu Eleccion Es: "))
     print("*" * 30)
     nivelacion = NivelacionDiferencialGeodesica()
