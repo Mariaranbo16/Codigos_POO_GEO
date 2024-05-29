d120fad2d21e12c772a3d618771ed02289721746
Mariana Arango <marangob@udistrital.edu.co>
2024-05-29 10:15:42 -0500

    vrs3

--- Coords_Inv/Frontend/ij.js
+++ Coords_Inv/Frontend/ij.js
@@ -1,11 +1,11 @@
 const tierra = document.getElementById("tierra");
 const barraLateral = document.querySelector(".barra-lateral");
-const spans = document.querySelectorAll(".menu-texto"); // Selecciona todos los elementos span con la clase menu-texto
+const spans = document.querySelectorAll(".menu-texto");
 
 tierra.addEventListener("click", () => {
     barraLateral.classList.toggle("mini-barra-lateral");
 
     spans.forEach((span) => {
-        span.classList.toggle("oculto"); // Alterna la clase oculto para cada elemento span
+        span.classList.toggle("oculto");
     });
 });
